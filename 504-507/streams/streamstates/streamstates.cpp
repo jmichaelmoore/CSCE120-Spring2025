4395dad28985d7b3f40f66329cf5452e0e3da8a6
Michael Moore <jmichael@tamu.edu>
2025-02-12 14:15:51 -0600

    prep for 02/12

---
+++ 504-507/streams/streamstates/streamstates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    int z = -99;
+    iss >> z;
+    cout << "z: " << z << endl;
+    cout << "iss.good(): " << boolalpha << iss.good() << endl;
+    cout << "iss.eof(): " << boolalpha << iss.eof() << endl;
+    cout << "iss.bad(): " << boolalpha << iss.bad() << endl;
+    cout << "iss.fail(): " << boolalpha << iss.fail() << endl; 
+}
