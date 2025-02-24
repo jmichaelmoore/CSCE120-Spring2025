1042c55057861dc4444484127e781e5bb59f17ec
Michael Moore <jmichael@tamu.edu>
2025-02-24 15:36:50 -0600

    prep for 02/24

---
+++ 504-507/passbyreference/1dvector/vectorFunctions.h
@@ -0,0 +1,24 @@
+// header guards
+#ifndef ARRAYFUNCTIONS_H
+#define ARRAYFUNCTIONS_H
+
+#include <vector>
+
+// functions to add
+//   loadRandom
+//   insert
+//   remove at index
+//   remove first of
+//   getMax // linear search
+//   getMin 
+//   count val
+//   print
+
+// these are declarations
+std::vector<int> loadRandom(size_t size);
+std::vector<int> insert(std::vector<int> v, int val, size_t index);
+std::vector<int> removeFirstOf(std::vector<int> v, int val); // first instance of
+int getMax(std::vector<int>);
+void print(std::vector<int>);
+
+#endif
