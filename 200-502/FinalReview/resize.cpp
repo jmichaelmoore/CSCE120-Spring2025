03fd92a8d4d33258fdf51313a2842f41887d4763
Michael Moore <jmichael@tamu.edu>
2025-04-28 18:30:14 -0500

    lecture 04/28

---
+++ 200-502/FinalReview/resize.cpp
@@ -0,0 +1,8 @@
+// pointer to a datatype / i.e. an array
+// will be on the heap
+// steps to resize
+// 1) create a new array on the heap of the appropriate size using a temp variable
+// 2) copy appropriate values from the current array to the new array
+// 3) delete old memory
+// 4) update variables for the array, size, capacity
+//      // e.g. point to 
